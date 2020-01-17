adee24c38d80658fbeb83c7bdacc606d157b9e46
Boaz <boazfrenkel@gmail.com>
2020-01-17 11:38:53 +0200

    Added the swift package manager to use SDWebImage to load images

--- StocksExample/Controller/StockGrid/GridViewCell.swift
+++ StocksExample/Controller/StockGrid/GridViewCell.swift
@@ -13,17 +13,10 @@ class GridViewCell: UICollectionViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var symbolLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
-    
-    var imagePath: String?
-    
+
     override func prepareForReuse() {
         super.prepareForReuse()
         self.imageView.image = nil
     }
-    
-    func setup(imagePath: String?, name: String, symbol: String) {
-        self.imagePath = imagePath
-        self.nameLabel.text = name
-        self.symbolLabel.text = symbol
-    }
+
 }
